9df64bba0d812e8626fe620fe4fd20b043534dc8
louieadamian <louie@adamian.net>
2020-10-01 20:03:34 -0400

    adds base site

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            {text:'Louie Adamian', link:'/'},
+            {text:'Portfolio', link:'/portfolio'},
+            {text:'Blog', link:'/blog'}
+        ]
+    }
+  }
---
+++ blog/index.md
@@ -0,0 +1,7 @@
+---
+title: "Blog"
+date: 2020-1-1
+blog_index: true
+---
+
+<BlogIndex />
