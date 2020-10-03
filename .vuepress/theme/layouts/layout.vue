83d2924ff6dc6d8a8ff46ee489b1295b243d7d1e
Louie Adamian <louie@adamian.net>
2020-10-03 02:03:04 -0400

    adds base theme

---
+++ .vuepress/theme/layouts/layout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="theme-container">
+        <div class="super-container">
+            <Nav/>
+            <div class="content-container">
+                <Content/> 
+            </div>    
+        </div>
+    </div>
+</template>
+<style lang="scss">
+@import '../styles/style.scss';
+</style>
