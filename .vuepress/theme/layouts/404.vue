613d29558dbf17cc2ee296b5351f52b997ce0e56
Louie Adamian <louie@adamian.net>
2020-10-03 02:17:46 -0400

    adds 404 page

---
+++ .vuepress/theme/layouts/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="theme-container">
+    <div class="theme-default-content">
+      <h1>404</h1>
+
+      <h2> the requested page was not found</h2>
+
+      <RouterLink to="/">
+        go home.
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
