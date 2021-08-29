31ae9e94804f06ae1b87056f39a63916a930762b
Louie Adamian <louie@adamian.net>
2021-08-28 20:42:43 -0400

    changes home to /

--- .vuepress/theme/components/Nav.vue
+++ .vuepress/theme/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="nav">
-            <router-link to="./">
+            <router-link to="/">
                 <h1 class="site-name">
                     Louis Adamian
                 </h1>
